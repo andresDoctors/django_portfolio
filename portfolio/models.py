5baf192640417c4f46eec630da6f7bc75b81a230
andresDoctors <101465588+andresDoctors@users.noreply.github.com>
2023-02-12 22:44:39 -0300

    Configured db to store projects
    - added Project class to '/portfolio/models.py'
    - created migration script with >python manage.py makemigrations
    - executed with > python manage.py migrate

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import CharField, URLField
+from django.db.models.fields.files import ImageField
 
-# Create your models here.
+
+# Define Project object we want tu preserve in our db
+class Project(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length=250)
+    image = ImageField(upload_to='portfolio/images/')
+    url = URLField(blank=True)
